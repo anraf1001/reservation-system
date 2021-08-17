86c99f957f1968974774108ad3813a89436a3162
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-17 19:44:55 +0200

    Changed main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
-#include "ReservationSystem.hpp"
+#include <iostream>
+#include <memory>
+
+#include "StandardSeat.hpp"
 
 int main() {
+    std::shared_ptr<Seat> seat = std::make_shared<StandardSeat>(123, false);
+    std::cout << std::boolalpha << seat->isTaken() << '\n';
+    std::cout << seat->getID() << '\n';
+
     return 0;
 }
