a3d88c0999dc80b5fb48a7f689f2b443c7886dc4
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-17 22:34:15 +0200

    Update main.cpp for testing seat classes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 
+#include "DisabledSeat.hpp"
 #include "StandardSeat.hpp"
+#include "VIPSeat.hpp"
 
 int main() {
-    std::shared_ptr<Seat> seat = std::make_shared<StandardSeat>(123, false);
-    std::cout << std::boolalpha << seat->isTaken() << '\n';
-    std::cout << seat->getID() << '\n';
+    using Seats = std::vector<std::shared_ptr<Seat>>;
+    Seats vec{std::make_shared<StandardSeat>(123, false),
+              std::make_shared<VIPSeat>(345, true),
+              std::make_shared<DisabledSeat>(678, false)};
+
+    for (const auto& seat : vec) {
+        std::cout << "Seat " << seat->getID() << " Type: " << seat->getSymbol() << '\n';
+    }
 
     return 0;
 }
