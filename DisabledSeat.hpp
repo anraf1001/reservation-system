da0c3dd81cfdf71c04a38929c8305b5d813943c2
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-18 13:24:38 +0200

    Added price to seats classes

--- DisabledSeat.hpp
+++ DisabledSeat.hpp
@@ -3,9 +3,10 @@
 #include "Seat.hpp"
 
 constexpr char disabledSeatSymbol = 'D';
+constexpr unsigned int disabledPrice = 250;
 
 class DisabledSeat : public Seat {
 public:
     DisabledSeat(unsigned int id, bool isTaken)
-        : Seat(disabledSeatSymbol, id, isTaken) {}
+        : Seat(disabledSeatSymbol, id, disabledPrice, isTaken) {}
 };
--- VIPSeat.hpp
+++ VIPSeat.hpp
@@ -3,9 +3,10 @@
 #include "Seat.hpp"
 
 constexpr char vipSeatSymbol = 'V';
+constexpr unsigned int vipPrice = 1000;
 
 class VIPSeat : public Seat {
 public:
     VIPSeat(unsigned int id, bool isTaken)
-        : Seat(vipSeatSymbol, id, isTaken) {}
+        : Seat(vipSeatSymbol, id, vipPrice, isTaken) {}
 };
