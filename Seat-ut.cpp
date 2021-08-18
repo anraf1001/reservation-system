32786a2fd52fdf5a26e9e43540447d467d0b9436
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-18 11:37:31 +0200

    Seats tests refactoring

--- Seat-ut.cpp
+++ Seat-ut.cpp
@@ -8,28 +8,53 @@
 #include "StandardSeat.hpp"
 #include "VIPSeat.hpp"
 
-TEST(SeatTests, shouldCreateDifferentTypesOfSeats) {
-    constexpr unsigned int standardID = 123;
-    constexpr unsigned int vipID = 456;
-    constexpr unsigned int disabledID = 789;
-    constexpr bool standardStatus = false;
-    constexpr bool vipStatus = true;
-    constexpr bool disabledStatus = false;
+constexpr unsigned int standardID = 123;
+constexpr unsigned int vipID = 456;
+constexpr unsigned int disabledID = 789;
+constexpr bool standardStatus = false;
+constexpr bool vipStatus = true;
+constexpr bool disabledStatus = false;
 
-    std::vector<std::shared_ptr<Seat>> seatsVec{
-        std::make_shared<StandardSeat>(standardID, standardStatus),
-        std::make_shared<VIPSeat>(vipID, vipStatus),
-        std::make_shared<DisabledSeat>(disabledID, disabledStatus)};
+struct SeatTests : ::testing::Test {
+    SeatTests() {
+        seatsVec.push_back(std::make_shared<StandardSeat>(standardID, standardStatus));
+        seatsVec.push_back(std::make_shared<VIPSeat>(vipID, vipStatus));
+        seatsVec.push_back(std::make_shared<DisabledSeat>(disabledID, disabledStatus));
+    }
 
-    ASSERT_EQ(seatsVec[0]->getID(), standardID);
-    ASSERT_EQ(seatsVec[1]->getID(), vipID);
-    ASSERT_EQ(seatsVec[2]->getID(), disabledID);
+    std::vector<std::shared_ptr<Seat>> seatsVec;
+};
 
-    ASSERT_EQ(seatsVec[0]->isTaken(), standardStatus);
-    ASSERT_EQ(seatsVec[1]->isTaken(), vipStatus);
-    ASSERT_EQ(seatsVec[2]->isTaken(), disabledStatus);
+TEST_F(SeatTests, shouldCreateDifferentTypesOfSeats) {
+    EXPECT_EQ(seatsVec[0]->getID(), standardID);
+    EXPECT_EQ(seatsVec[1]->getID(), vipID);
+    EXPECT_EQ(seatsVec[2]->getID(), disabledID);
 
-    ASSERT_EQ(seatsVec[0]->getSymbol(), standardSeatSymbol);
-    ASSERT_EQ(seatsVec[1]->getSymbol(), vipSeatSymbol);
-    ASSERT_EQ(seatsVec[2]->getSymbol(), disabledSeatSymbol);
+    EXPECT_EQ(seatsVec[0]->isTaken(), standardStatus);
+    EXPECT_EQ(seatsVec[1]->isTaken(), vipStatus);
+    EXPECT_EQ(seatsVec[2]->isTaken(), disabledStatus);
+
+    EXPECT_EQ(seatsVec[0]->getSymbol(), standardSeatSymbol);
+    EXPECT_EQ(seatsVec[1]->getSymbol(), vipSeatSymbol);
+    EXPECT_EQ(seatsVec[2]->getSymbol(), disabledSeatSymbol);
+}
+
+TEST_F(SeatTests, shouldTakeSeats) {
+    for (auto& seat : seatsVec) {
+        seat->take();
+    }
+
+    for (const auto& seat : seatsVec) {
+        EXPECT_TRUE(seat->isTaken());
+    }
+}
+
+TEST_F(SeatTests, shouldFreeSeats) {
+    for (auto& seat : seatsVec) {
+        seat->free();
+    }
+
+    for (const auto& seat : seatsVec) {
+        EXPECT_FALSE(seat->isTaken());
+    }
 }
