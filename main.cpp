38d669c11a656f897abc1a2249f89b204598c8c6
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-24 13:19:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 
 int main() {
     using Seats = std::vector<std::shared_ptr<Seat>>;
-    Seats vec{std::make_shared<StandardSeat>(123, false),
-              std::make_shared<VIPSeat>(345, true),
-              std::make_shared<DisabledSeat>(678, false)};
+    Seats vec{std::make_shared<StandardSeat>(123),
+              std::make_shared<VIPSeat>(345),
+              std::make_shared<DisabledSeat>(678)};
 
     for (const auto& seat : vec) {
         std::cout << "Seat " << seat->getID() << " Type: " << seat->getSymbol() << '\n';
