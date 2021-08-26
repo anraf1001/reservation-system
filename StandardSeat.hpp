5d57673d788229bcb2a1b59d6149670d030c21f9
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-26 18:38:23 +0200

    Changed getPrice to constexpr method

--- StandardSeat.hpp
+++ StandardSeat.hpp
@@ -9,7 +9,9 @@ constexpr unsigned int standardPrice = 370;
 class StandardSeat : public Seat {
 public:
     StandardSeat(unsigned int id, std::shared_ptr<Person> owner) noexcept
-        : Seat(standardSeatSymbol, id, standardPrice, owner) {}
+        : Seat(standardSeatSymbol, id, owner) {}
     StandardSeat(unsigned int id) noexcept
-        : Seat(standardSeatSymbol, id, standardPrice) {}
+        : Seat(standardSeatSymbol, id) {}
+
+    constexpr unsigned int getPrice() const noexcept override { return standardPrice; }
 };
