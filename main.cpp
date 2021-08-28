b83ba8b53ac9a21a200c2ddc1dcdeea926085da5
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-28 11:21:27 +0200

    Update main.cpp - printing seats

--- main.cpp
+++ main.cpp
@@ -16,9 +16,19 @@ int main() {
     for (const auto& seat : vec) {
         std::cout << "Seat " << seat->getID() << " Type: " << seat->getSymbol() << " Price: " << seat->getPrice() << '\n';
     }
+    for (const auto& seat : vec) {
+        std::cout << *seat;
+    }
+    std::cout << '\n';
 
     Person person{"Jan", "Kowalski", "123456789", "email@email.com", "20271854937"};
     std::cout << person.getAge() << '\n';
+
+    vec[1]->take(std::make_shared<Person>("Jan", "Kowalski", "123456789", "email@email.com", "20271854937"));
+    for (const auto& seat : vec) {
+        std::cout << *seat;
+    }
+    std::cout << '\n';
 
     return 0;
 }
