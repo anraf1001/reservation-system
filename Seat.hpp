64c258ebf52e65dd96c7f1369afe0c1b6d2e3a8d
Andrzej Rafalski <anraf1001@gmail.com>
2021-08-28 11:26:18 +0200

    Deleting whitespace in seats printing function

--- Seat.hpp
+++ Seat.hpp
@@ -30,9 +30,9 @@ protected:
 
 std::ostream& operator<<(std::ostream& out, const Seat& seat) {
     if (seat.isTaken()) {
-        out << ':' << seat.getSymbol() << ": ";
+        out << ':' << seat.getSymbol() << ':';
     } else {
-        out << '[' << seat.getSymbol() << "] ";
+        out << '[' << seat.getSymbol() << ']';
     }
     return out;
 }
