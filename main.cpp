0c8902a1235445004bcbd4274d72d2c88da59f02
Andrzej Rafalski <anraf1001@gmail.com>
2021-09-15 19:11:36 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,8 @@
-#include <iostream>
-#include <memory>
-#include <vector>
-
-#include "DisabledSeat.hpp"
-#include "Person.hpp"
-#include "StandardSeat.hpp"
-#include "VIPSeat.hpp"
+#include "ReservationSystem.hpp"
 
 int main() {
-    using Seats = std::vector<std::shared_ptr<Seat>>;
-    Seats vec{std::make_shared<StandardSeat>(123),
-              std::make_shared<VIPSeat>(345),
-              std::make_shared<DisabledSeat>(678)};
-
-    for (const auto& seat : vec) {
-        std::cout << "Seat " << seat->getID() << " Type: " << seat->getSymbol() << " Price: " << seat->getPrice() << '\n';
-    }
-    for (const auto& seat : vec) {
-        std::cout << *seat << ' ';
-    }
-    std::cout << '\n';
-
-    Person person{"Jan", "Kowalski", "123456789", "email@email.com", "20271854937"};
-    std::cout << person.getAge() << '\n';
-
-    vec[1]->take(std::make_shared<Person>("Jan", "Kowalski", "123456789", "email@email.com", "20271854937"));
-    for (const auto& seat : vec) {
-        std::cout << *seat << ' ';
-    }
-    std::cout << '\n';
+    ReservationSystem rs{};
+    rs.printStadium();
 
     return 0;
 }
