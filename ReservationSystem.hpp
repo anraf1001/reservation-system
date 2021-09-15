4a8e708bab5bf3f7f828d3ba10a4c9b789181d33
Andrzej Rafalski <anraf1001@gmail.com>
2021-09-15 19:11:16 +0200

    Parsing and printing seats

--- ReservationSystem.hpp
+++ ReservationSystem.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <boost/json.hpp>
 #include <filesystem>
 #include <memory>
 #include <vector>
 
 #include "Person.hpp"
 #include "Seat.hpp"
+#include "SeatPtrFactory.hpp"
 
 namespace fs = std::filesystem;
+namespace json = boost::json;
 
 using Persons = std::vector<std::shared_ptr<Person>>;
 using Row = std::vector<std::unique_ptr<Seat>>;
@@ -17,8 +20,16 @@ class ReservationSystem {
 public:
     ReservationSystem();
 
+    void printStadium() const;
+
 private:
     void readClientsDatabase(const fs::path& database);
+    void readSeatsDatabase(const fs::path& database, SeatPtrFactory& ptrFactory);
 
+    std::unique_ptr<Seat> seatFromJsonValue(const json::value& jv, SeatPtrFactory& ptrFactory);
+    Row rowFromJsonValue(const json::value& jv, SeatPtrFactory& ptrFactory);
+    Stadium stadiumFromJson(const json::value& jv, SeatPtrFactory& ptrFactory);
+
+    Stadium stadium_;
     Persons clients_;
 };
