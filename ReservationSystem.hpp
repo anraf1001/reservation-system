e7bbd65a15216ea56fc9222d48fc7ade5c39f721
Andrzej Rafalski <anraf1001@gmail.com>
2021-09-15 14:53:37 +0200

    Reading clients to system

--- ReservationSystem.hpp
+++ ReservationSystem.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <filesystem>
 #include <memory>
 #include <vector>
 
+#include "Person.hpp"
 #include "Seat.hpp"
 
+namespace fs = std::filesystem;
+
+using Persons = std::vector<std::shared_ptr<Person>>;
 using Row = std::vector<std::unique_ptr<Seat>>;
 using Stadium = std::vector<Row>;
 
@@ -13,4 +18,7 @@ public:
     ReservationSystem();
 
 private:
+    void readClientsDatabase(const fs::path& database);
+
+    Persons clients_;
 };
